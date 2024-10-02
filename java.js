91837f560b148ae5a8ee650cd7053c8871f95849
oscaraabroe <oscar.aabroe@live.dk>
2024-10-02 14:11:45 +0200

    Meget af opgaven blev ikke nået. Jeg brugte alt for meget tid på at se youtube guides om @mixin og især at oprette variabler. Ingen af de 2 ting endte jeg med at få løst. Der må være noget galt med min opsætning, siden SCSS dukker op i browseren, men uden værdier.

--- java.js
+++ java.js
@@ -1,12 +1,14 @@
+let dropdownItems = document.getElementById(".dropdown-items")
+let dropdownMenu = document.getElementById("#dropdown--menu")
 document.querySelector(".dropdown");
-document.addEventListener('click', showHideMenu)
-var dropdownItems = document.getElementById(".dropdown-items")
+dropdownMenu.addEventListener('click', showHideMenu)
+
 
 function showHideMenu(){
     if (dropdownItems.style.display = none){
         dropdownItems.style.display = block;
-        
+
     } else{
         dropdownItems.style.display = none
     }
-}
+};
